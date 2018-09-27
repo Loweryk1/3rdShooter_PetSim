aac020e1128f952b496bc7bd16586f97079b1b83
Kyle Lowery <Loweryk1@ferris.edu>
2018-09-26 20:39:15 -0400

    Made the Camera 3rd Person Behind the Player
    +ThirdPersonCamera.cs , for the Main Camera to use to follow the player.

--- Assets/Scripts/FirstPersonMovement.cs
+++ Assets/Scripts/FirstPersonMovement.cs
@@ -6,24 +6,17 @@ using UnityEngine;
 public class FirstPersonMovement : MonoBehaviour {
 
     CharacterController pawn;
-    Camera cam;
 
     public float speed = 5;
-    public float lookSensitivityX = 5;
-    public float lookSensitivityY = 5;
-    public bool invertLookX = false;
-    public bool invertLookY = true;
 
 	// Use this for initialization
 	void Start () {
         pawn = GetComponent<CharacterController>();
-        cam = GetComponentInChildren<Camera>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        LookAround();
         MoveAround();
     }
 
@@ -36,17 +29,5 @@ public class FirstPersonMovement : MonoBehaviour {
         velocity += transform.forward * v * speed;
         velocity += transform.right * h * speed;
         pawn.SimpleMove(velocity);
-    }
-
-    private void LookAround()
-    {
-        float lookX = Input.GetAxis("Mouse X") * (invertLookX ? -1 : 1) * lookSensitivityX;
-        float lookY = Input.GetAxis("Mouse Y") * (invertLookY ? -1 : 1) * lookSensitivityY;
-
-        transform.Rotate(0, lookX, 0);
-        if (lookY < 0 && cam.transform.localEulerAngles.x > 80) return;
-        if (lookY > 0 && cam.transform.localEulerAngles.x < -80) return;
-
-        cam.transform.Rotate(lookY, 0, 0);
     }
 }
